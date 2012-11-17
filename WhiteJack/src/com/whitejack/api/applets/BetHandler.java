e4eea63e9b87cbdac1c2dd0fe8175820ba083575
kevin <kcat3000@gmail.com>
2012-11-17 17:39:51 -0600

    Fixed package declaration

--- WhiteJack/src/com/whitejack/api/applets/BetHandler.java
+++ WhiteJack/src/com/whitejack/api/applets/BetHandler.java
@@ -1,21 +1,16 @@
+package com.whitejack.api.applets;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
 import java.net.URL;
+
 import javax.swing.ImageIcon;
 import javax.swing.JApplet;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
-package com.whitejack.api.applets;
 
 public class BetHandler extends JApplet 
 {

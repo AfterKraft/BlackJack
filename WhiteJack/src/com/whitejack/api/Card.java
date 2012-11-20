0ca3e66f2c5e8e2369a1fb4040f37254fe853d2b
kevin naruse <kcat3000@gmail.com>
2012-11-19 22:00:07 -0600

    Update WhiteJack/src/com/whitejack/api/Card.java

--- WhiteJack/src/com/whitejack/api/Card.java
+++ WhiteJack/src/com/whitejack/api/Card.java
@@ -9,60 +9,79 @@ import com.whitejack.api.applets.BetHandler;
 public class Card {
 
 	private int cardID;
-	private String suits[], suit;
-	private String ranks[], rank;
+	private int value;
+	protected String suits[];
+	private String suit;
+	protected String ranks[];
+	private String rank;
 	private URL url;
 	private String path;
 	private ImageIcon icon;
-
-	public Card() {
+	public Card()
+	{
 	}
-
-	public Card(int i) {
-		String suit = suits[i / 13];
-		String rank = ranks[i % 13];
+	public Card(int i) 
+	{
+    	String suit = suits[i / 13];
+    	String rank = ranks[i % 13];
 	}
-
-	public void setURL() {
+	private void setPath(String string)
+	{
+		this.path = string;
+	}
+	private void setURL()
+	{
 		path = "/com/whitejack/images/GameTable/";
 		path += rank + suit;
 		url = BetHandler.class.getResource(path);
 		icon = new ImageIcon(url);
 	}
-
-	public void getURL() {
-
+	public URL getURL()
+	{
+    	return url;
 	}
-
-	public String getSuit() {
+	public String getSuit()
+	{
 		String suit = suits[cardID / 13];
 		return suit;
 	}
-
-	public void setSuit() {
+	private void setSuit()
+	{	
 	}
-
-	public String getRank() {
+	public String getRank()
+	{
 		return ranks[cardID % 13];
 	}
-
-	public void setCardID(int num) {
+	public void setCardID(int num) 
+	{
 		cardID = num;
 	}
-
-	public int getCardID() {
+	public int getCardID()
+	{
 		return cardID;
 	}
-
-	public int getNumber() {
+	public int getNumber() 
+	{
 		return cardID;
 	}
-
-	public void setSuit(String suit) {
-		this.suit = suit;
+	public void setSuit(String suit) 
+	{
+		this.suit=suit;
 	}
-
-	public int getValue() {
-		return 0;
+	public void displayImage(int cardID)
+	{
+		// pull the card image from url and show on a gui
+	}
+	public void hideImage()
+	{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+ 		
+	}
+	public int getValue()
+	{
+		return value;
+	}
+	private void setValue(int value)
+	{
+		this.value = value;
 	}
 }
